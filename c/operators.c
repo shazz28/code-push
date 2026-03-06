4a44fec4fda5f9a430b8d061459b8b9119c998ba
shazz28 <shashankshazz28@gmail.com>
2026-03-06 17:39:46 +0530

    day 2 march 6th

---
+++ c/operators.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+    int a = 10;
+    int b = 20;
+    int add, sub,mul, div, mod, z=10,x=10,y=10,w=10;
+
+    add = a+b;
+    sub = b-a;
+    mul = a*b;
+    div = b/a;
+    mod =a%b;
+
+    //augmented assignment operator
+    z+=2;
+    x-=2;
+    y*=5;
+    w/=2;
+
+    printf("the addition is %d\n", add);
+    printf("the subtraction is %d\n",sub);
+    printf("the multiplication is %d\n",mul);
+    printf("the division is %d\n",div);
+    printf("the modulous is %d\n",mod);
+
+    //output for augmented assignment operator
+    printf("the addition is %d\n", z);
+    printf("the subtraction is %d\n",x);
+    printf("the multiplication is %d\n",y);
+    printf("the division is %d\n",w);
+
+
+    // \n
+}
